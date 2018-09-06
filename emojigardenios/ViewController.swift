90a151a755d0419741b7ce4e4f722292913d37e8
Anne Cahalan <Anne@annes-macbook-pro.local>
2018-09-05 20:51:25 -0400

    Initial Commit

---
+++ emojigardenios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  emojigardenios
+//
+//  Created by Anne Cahalan on 9/5/18.
+//  Copyright © 2018 Anne Cahalan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
