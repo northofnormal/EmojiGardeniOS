f0eccf331760d0a8b90bdbdf72e7193bae7d0724
Anne Cahalan <Anne@Annes-MBP.lan>
2018-09-13 11:51:18 -0400

    set up ui and added cocoapod for color picker

--- emojigardenios/MakeYourOwnThemeViewController.swift
+++ emojigardenios/MakeYourOwnThemeViewController.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2018 Anne Cahalan. All rights reserved.
 //
 
+import IGColorPicker
 import UIKit
 
 class MakeYourOwnThemeViewController: UIViewController {
@@ -13,8 +14,17 @@ class MakeYourOwnThemeViewController: UIViewController {
     @IBOutlet var contextField: UITextField!
     @IBOutlet var friendsField: UITextField!
     @IBOutlet var flairField: UITextField!
+    @IBOutlet var colorPickerView: ColorPickerView!
     
     var userGeneratedTheme: Theme?
+    var selectedColor: UIColor?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        colorPickerView.delegate = self
+        colorPickerView.layoutDelegate = self
+    }
 
     @IBAction func goBackButtonPressed() {
         dismiss(animated: true, completion: nil)
@@ -25,7 +35,7 @@ class MakeYourOwnThemeViewController: UIViewController {
         let friendsArray = convertToArray(friendsField.text)
         let flairArray = convertToArray(flairField.text)
         
-        userGeneratedTheme = Theme(context: contextArray, friends: friendsArray, flair: flairArray, backgroundColor: <#T##UIColor#>)
+        userGeneratedTheme = Theme(context: contextArray, friends: friendsArray, flair: flairArray, backgroundColor: selectedColor ?? UIColor.white)
     }
     
     private func convertToArray(_ string: String?) -> [String] {
@@ -36,7 +46,37 @@ class MakeYourOwnThemeViewController: UIViewController {
         return newArray
     }
     
+    // restrict keyboard and/or entry to emoji
     // dismiss keyboard
-    // set up background color ui
+    // set up rows/columns UI
+    // set up rows/columns logic
 
+}
+
+extension MakeYourOwnThemeViewController: ColorPickerViewDelegate {
+    
+    func colorPickerView(_ colorPickerView: ColorPickerView, didSelectItemAt indexPath: IndexPath) {
+        selectedColor = colorPickerView.colors[indexPath.item]
+        view.backgroundColor = selectedColor
+    }
+
+}
+
+extension MakeYourOwnThemeViewController: ColorPickerViewDelegateFlowLayout {
+    
+    func colorPickerView(_ colorPickerView: ColorPickerView, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 40, height: 40)
+    }
+    
+    func colorPickerView(_ colorPickerView: ColorPickerView, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 10
+    }
+    
+    func colorPickerView(_ colorPickerView: ColorPickerView, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 10
+    }
+    
+    func colorPickerView(_ colorPickerView: ColorPickerView, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 5, left: 5, bottom: 5, right: 5)
+    }
 }
